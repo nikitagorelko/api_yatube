5c527f1da4a0a2691d900dd371949acf58d71afb
Горелько Никита <nikita.gorelko@mail.ru>
2023-04-25 03:10:11 +0300

    second raw version

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,27 +1,30 @@
+from posts.models import Comment, Group, Post
 from rest_framework import serializers
-
-from posts.models import Post, Group, Comment
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('id', 'title', 'slug',)
+        fields = ('id', 'title', 'slug', 'description')
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.PrimaryKeyRelatedField(
-        read_only=True, default=serializers.CurrentUserDefault())
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
     group = GroupSerializer(read_only=True)
+
     class Meta:
         model = Post
         fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.PrimaryKeyRelatedField(
-        read_only=True, default=serializers.CurrentUserDefault())
-    post = PostSerializer(read_only=True)
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
     class Meta:
         model = Comment
         fields = '__all__'
+        read_only_fields = ('author', 'post')
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,12 +1,15 @@
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, GroupViewSet, CommentViewSet
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+
+from .views import CommentViewSet, GroupViewSet, PostViewSet
 
 router = DefaultRouter()
 router.register('posts', PostViewSet)
 router.register('groups', GroupViewSet)
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments'
+)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
